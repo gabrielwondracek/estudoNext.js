7485cffb849e188015f9a2edb4204cc6727c917f
gabrielwondracek <gabihw.2005@gmail.com>
2025-03-08 01:50:19 -0300

    Aprendi a mexer com metadata SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
-import { Header } from '../components/header';
+import { Header } from "../components/header";
 import "./globals.css";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Aprendendo Next.js",
+  description: "Aprendendo Next.js",
+};
 
 export default function RootLayout({
   children,
@@ -9,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body
-        className={`antialiased`}
-      >
+      <body className={`antialiased`}>
         <Header />
         {children}
       </body>
